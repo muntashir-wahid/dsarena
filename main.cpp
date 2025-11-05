73aff5109283f8fdab203e2be6c45e97eb33ae9e
muntashir-wahid <muntashir.w.riseuplabs@gmail.com>
2025-11-05 17:59:08 +0600

    learning STL

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include <bits/stdtr1c++.h>
 
 using namespace std;
 
 int main() {
-    cout << "Let's Learn Data Structures with c++" << endl;
+    /*
+    pair<int, int> numsPair = {1, 3};
+
+    cout << "First element of the Pair: " << numsPair.first << ", and second element of the pair: " << numsPair.second << endl;
+
+    pair<int, int> numPairs[] = {{2, 3}, {2, 5}, {3, 6}};
+
+    for(int i = 0; i < 3; i++) {
+        cout << "First: " << numPairs[i].first << ", second: " << numPairs[i].second << endl;
+    } 
+    */
+
+    vector<int> numsVec(10, 50);
+
+    for(vector<int>::iterator it = numsVec.begin(); it != numsVec.end(); it++) {
+        cout << *it << endl;
+    }
+
     return 0;
 }
